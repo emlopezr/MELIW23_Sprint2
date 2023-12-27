83e9aad3586e2ddc573f5863f9c4d183532abc95
jhongarcia85 <jhon.garciagarcia@mercadolibre.com.co>
2023-12-27 14:43:32 -0500

    (Feature): Adding tests for result is zero and Seller not exist

--- src/test/java/com/example/be_java_hisp_w23_g3/service/user/UserServiceImplTests.java
+++ src/test/java/com/example/be_java_hisp_w23_g3/service/user/UserServiceImplTests.java
@@ -5,6 +5,7 @@ import com.example.be_java_hisp_w23_g3.entity.Seller;
 import com.example.be_java_hisp_w23_g3.entity.User;
 import com.example.be_java_hisp_w23_g3.exception.InvalidOrderException;
 import com.example.be_java_hisp_w23_g3.exception.NotFoundException;
+import com.example.be_java_hisp_w23_g3.dto.response.MessageResponseDTO;
 import com.example.be_java_hisp_w23_g3.repository.seller.SellerRepository;
 import com.example.be_java_hisp_w23_g3.repository.user.UserRepository;
 import com.example.be_java_hisp_w23_g3.util.SellerTestDataBuilder;
@@ -15,14 +16,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTests {
@@ -264,5 +264,33 @@ class UserServiceImplTests {
         when(userRepository.read(anyLong())).thenReturn(Optional.empty());
 
         assertThrows(NotFoundException.class, () -> service.getFollowedSellersList(1L, null));
+
+    }
+
+    @Test
+    void followSeller_shouldWorkWhenSellerExists() {
+        Long userId = 1L;
+        Long sellerIdToFollow = 9L;
+        User user = new UserTestDataBuilder().userByDefault().withId(userId).withUsername("Lisandro").build();
+        Seller sellerToFollow = new SellerTestDataBuilder().sellerByDefault().withId(sellerIdToFollow).withUsername("Karla").build();
+
+        when(userRepository.read(userId)).thenReturn(Optional.ofNullable(user));
+        when(sellerRepository.read(sellerIdToFollow)).thenReturn(Optional.ofNullable(sellerToFollow));
+        MessageResponseDTO respond = service.followSeller(userId, sellerIdToFollow);
+
+        assertEquals(user, sellerToFollow.getFollower().stream().findFirst().get());
+        assertEquals(sellerToFollow, user.getFollowing().stream().findFirst().get());
+        assertEquals(new MessageResponseDTO("Following a new Seller!"), respond);
+        verify(sellerRepository,times(1)).read(sellerIdToFollow);
+        verify(userRepository, times(1)).read(userId);
+    }
+
+    @Test
+    void followSeller_shouldNotWorkWhenSellerDoesNotExist(){
+        Long userId = 1L;
+        Long sellerIdToFollow = 999L;
+        when(sellerRepository.read(sellerIdToFollow)).thenThrow(NotFoundException.class);
+        assertThrows(NotFoundException.class,() -> service.followSeller(userId,sellerIdToFollow));
+        verify(sellerRepository,times(1)).read(sellerIdToFollow);
     }
 }
