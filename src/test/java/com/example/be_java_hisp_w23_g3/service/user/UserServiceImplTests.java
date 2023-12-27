4525cecb59e5868cbd1af8ce34248a0d3ebf6f00
jhongarcia85 <jhon.garciagarcia@mercadolibre.com.co>
2023-12-27 14:20:53 -0500

    (Feature): Adding test for count of users following of specific seller.

--- src/test/java/com/example/be_java_hisp_w23_g3/service/user/UserServiceImplTests.java
+++ src/test/java/com/example/be_java_hisp_w23_g3/service/user/UserServiceImplTests.java
@@ -1,11 +1,21 @@
 package com.example.be_java_hisp_w23_g3.service.user;
 
+import com.example.be_java_hisp_w23_g3.entity.Seller;
+import com.example.be_java_hisp_w23_g3.entity.User;
 import com.example.be_java_hisp_w23_g3.repository.seller.SellerRepository;
 import com.example.be_java_hisp_w23_g3.repository.user.UserRepository;
+import com.example.be_java_hisp_w23_g3.util.SellerTestDataBuilder;
+import com.example.be_java_hisp_w23_g3.util.UserTestDataBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTests {
@@ -18,6 +28,24 @@ class UserServiceImplTests {
 
     @InjectMocks
     UserServiceImpl service;
+
+    @Test
+    void getFollowersCount_getResultOfNumberOfUsersWhoFollowingSpecificSeller(){
+        Long param = 1L;
+
+        Seller seller = new SellerTestDataBuilder()
+                .withId(param)
+                .sellerWithFollowers()
+                .build();
+
+        Integer expected = seller.getFollower().size();
+
+        when(sellerRepository.read(param)).thenReturn(Optional.of(seller));
+
+        Integer actual = service.getFollowersCount(param).getFollowersCount();
+
+        Assertions.assertEquals(expected, actual);
+    }
 
 
 }
