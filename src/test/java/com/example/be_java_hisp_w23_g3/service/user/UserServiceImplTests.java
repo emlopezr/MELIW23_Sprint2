a44c2a9ba574601292656b37b5ee7faaf53aeea5
dianisrmz <diana.ramirezgarcia@mercadolibre.com.mx>
2023-12-27 12:38:45 -0600

    (Feat): Added followSellerTest happy path - Checks user is added to followed list of seller - Checks seller is added to following list of user - Checks sellerRepository read() is called - Checks userRepository read() is called - Checks response message

--- src/test/java/com/example/be_java_hisp_w23_g3/service/user/UserServiceImplTests.java
+++ src/test/java/com/example/be_java_hisp_w23_g3/service/user/UserServiceImplTests.java
@@ -1,11 +1,24 @@
 package com.example.be_java_hisp_w23_g3.service.user;
 
+import com.example.be_java_hisp_w23_g3.dto.response.MessageResponseDTO;
+import com.example.be_java_hisp_w23_g3.entity.Seller;
+import com.example.be_java_hisp_w23_g3.entity.User;
+import com.example.be_java_hisp_w23_g3.exception.NotFoundException;
 import com.example.be_java_hisp_w23_g3.repository.seller.SellerRepository;
+import com.example.be_java_hisp_w23_g3.repository.seller.SellerRepositoryImpl;
 import com.example.be_java_hisp_w23_g3.repository.user.UserRepository;
+import com.example.be_java_hisp_w23_g3.util.SellerTestDataBuilder;
+import com.example.be_java_hisp_w23_g3.util.UserTestDataBuilder;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTests {
@@ -19,5 +32,22 @@ class UserServiceImplTests {
     @InjectMocks
     UserServiceImpl service;
 
+    @Test
+    void followSeller_shouldWorkWhenSellerExists(){
+        Long userId = 1L;
+        Long sellerIdToFollow = 9L;
+        User user = new UserTestDataBuilder().userByDefault().withId(userId).withUsername("Lisandro").build();
+        Seller sellerToFollow = new SellerTestDataBuilder().sellerByDefault().withId(sellerIdToFollow).withUsername("Karla").build();
+
+        when(userRepository.read(userId)).thenReturn(Optional.ofNullable(user));
+        when(sellerRepository.read(sellerIdToFollow)).thenReturn(Optional.ofNullable(sellerToFollow));
+        MessageResponseDTO respond =service.followSeller(userId,sellerIdToFollow);
+
+        assertEquals(user, sellerToFollow.getFollower().stream().findFirst().get());
+        assertEquals(sellerToFollow, user.getFollowing().stream().findFirst().get());
+        assertEquals(new MessageResponseDTO("Following a new Seller!"), respond);
+        verify(sellerRepository,times(1)).read(sellerIdToFollow);
+        verify(userRepository, times(1)).read(userId);
+    }
 
 }
